6b3d4a2c5eb5521a238a4b0dab0b25c6842a6b44
ttubis1 <ttubis1@gmail.com>
2026-02-15 15:07:22 +0200

    Enable CORS and ignore jobs.db

--- Program.cs
+++ Program.cs
@@ -57,9 +57,20 @@ else
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 
 var app = builder.Build();
+app.UseCors("AllowAll");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
